dfba6024e9583aef829c6a2765b8f0a09ef71bb4
rlichti <rlichti@redhat.com>
2022-10-21 21:54:02 +0200

    WIP: inital check in of the skeleton. Demo code needs to removed yet.

---
+++ kp-librarian/src/main/java/de/kaiserpfalzedv/office/library/librarian/GreetingConfig.java
@@ -0,0 +1,12 @@
+package de.kaiserpfalzedv.office.library.librarian;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
---
+++ kp-library/src/main/java/de/kaiserpfalzedv/office/library/library/GreetingConfig.java
@@ -0,0 +1,12 @@
+package de.kaiserpfalzedv.office.library.library;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
